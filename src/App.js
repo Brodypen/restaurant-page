9b8ff7bc1b3ca9ceebbe01b0b027f5a0ec36c352
BrodyLinux <Frostwizard7987@gmail.com>
2022-06-11 13:38:22 -0500

    Clean up starter files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="h-screen bg-blue-100">
+      <header className="bg-blue-700">
+        Hello World!
       </header>
     </div>
   );
