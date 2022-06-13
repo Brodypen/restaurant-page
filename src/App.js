94be8bcf9ae47d78e1ca0856cd7e47333da26fe8
BrodyLinux <Frostwizard7987@gmail.com>
2022-06-13 09:33:26 -0500

    Delete extra files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="h-screen bg-blue-100">
-      <header className="bg-blue-700">
+    <div className="h-screen">
+      <header className="absolute top-0 left-0 w-screen h-20 bg-black bg-opacity-70 p-4">
         Hello World!
       </header>
+      <section className="h-screen bg-slate-50">
+
+
+      </section>
+      <section className="h-screen bg-slate-50">
+
+
+      </section>
+
     </div>
   );
 }
